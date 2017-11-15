872dea8334443f55b1e00fb7441b836593c98ff2
slepher <cxyfree@gmail.com>
2017-11-15 13:19:44 +0800

    remove left/0, right/0, either/3 in either

--- src/either.erl
+++ src/either.erl
@@ -30,9 +30,8 @@
 -export([fail/1]).
 -export([run_nargs/0, run_m/2]).
 -export([run/1]).
--export([either/2, either/3]).
--export([swap/0]).
--export([left/0, left/1, right/0, right/1]).
+-export([left/1, right/1]).
+-export([either/2, swap/0]).
 
 -transform(#{patterns => [?TYPE], gbehaviours => [functor, applicative, monad, monad_fail]}).
 
@@ -91,31 +90,25 @@ run(#undetermined{} = UEA) ->
 run(Either) ->
     Either.
 
-either(FAC, FBC) ->
-    either(FAC, FBC, _).
-
-either(FAC, FBC, EAB) ->
-    case EAB of
-        {left, A} ->
-            FAC(A);
-        {right, B} ->
-            FBC(B)
-    end.
-
-swap() ->
-    either(right(), left()).
-
-left() ->
-    left(_).
-
 left(A) ->
     {left, A}.
 
-right() ->
-    right(_).
-
 right(B) ->
     {right, B}.
+
+either(FAC, FBC) ->
+    fun(EAB) ->
+            case EAB of
+                {left, A} ->
+                    FAC(A);
+                {right, B} ->
+                    FBC(B)
+            end
+    end.
+
+swap() ->
+    either(right(_), left(_)).
+
 
 %%--------------------------------------------------------------------
 %% @doc
