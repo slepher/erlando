794a46a28eb544da85da9960faa39938429496e8
slepher <cxyfree@gmail.com>
2017-11-22 00:40:34 +0800

    add test suite monad_fail_SUITE monad_state_SUITE

---
+++ test/monad_state_SUITE.erl
@@ -0,0 +1,188 @@
+%%%-------------------------------------------------------------------
+%%% @author Chen Slepher <slepheric@gmail.com>
+%%% @copyright (C) 2017, Chen Slepher
+%%% @doc
+%%%
+%%% @end
+%%% Created : 22 Nov 2017 by Chen Slepher <slepheric@gmail.com>
+%%%-------------------------------------------------------------------
+-module(monad_state_SUITE).
+
+%% Note: This directive should only be used in test suites.
+-compile(export_all).
+
+-include_lib("eunit/include/eunit.hrl").
+-include_lib("common_test/include/ct.hrl").
+
+%%--------------------------------------------------------------------
+%% TEST SERVER CALLBACK FUNCTIONS
+%%--------------------------------------------------------------------
+
+%%--------------------------------------------------------------------
+%%
+%% @doc
+%% Initialization before the suite.
+%%
+%% Config0 = Config1 = [tuple()]
+%%   A list of key/value pairs, holding the test case configuration.
+%% Reason = term()
+%%   The reason for skipping the suite.
+%%
+%% Note: This function is free to add any key/value pairs to the Config
+%% variable, but should NOT alter/remove any existing entries.
+%%
+%% @spec init_per_suite(Config) -> Config
+%% @end
+%%--------------------------------------------------------------------
+init_per_suite(Config) ->
+    erlando:start(),
+    Config.
+
+%%--------------------------------------------------------------------
+%% @doc
+%% Cleanup after the suite.
+%% Config - [tuple()]
+%%   A list of key/value pairs, holding the test case configuration.
+%%
+%% @spec end_per_suite(Config) -> _
+%% @end
+%%--------------------------------------------------------------------
+end_per_suite(_Config) ->
+    ok.
+
+%%--------------------------------------------------------------------
+%% @doc
+%% Initialization before each test case
+%%
+%% TestCase - atom()
+%%   Name of the test case that is about to be run.
+%% Config - [tuple()]
+%%   A list of key/value pairs, holding the test case configuration.
+%% Reason = term()
+%%   The reason for skipping the test case.
+%%
+%% Note: This function is free to add any key/value pairs to the Config
+%% variable, but should NOT alter/remove any existing entries.
+%%
+%% @spec init_per_testcase(TestCase, Config) -> Config
+%% @end
+%%--------------------------------------------------------------------
+init_per_testcase(_TestCase, Config) ->
+    Config.
+
+%%--------------------------------------------------------------------
+%% @doc
+%% Cleanup after each test case
+%%
+%% TestCase = atom()
+%%   Name of the test case that is finished.
+%% Config = [tuple()]
+%%   A list of key/value pairs, holding the test case configuration.
+%%
+%% @spec end_per_testcase(TestCase, Config) -> _
+%% @end
+%%--------------------------------------------------------------------
+end_per_testcase(_TestCase, _Config) ->
+    ok.
+
+%%--------------------------------------------------------------------
+%% @doc
+%% Returns a description of the test suite when
+%% Clause == doc, and a test specification (list
+%% of the conf and test cases in the suite) when
+%% Clause == suite.
+%% Returns a list of all test cases in this test suite
+%%
+%% Clause = doc | suite
+%%   Indicates expected return value.
+%% Descr = [string()] | []
+%%   String that describes the test suite.
+%% Spec = [TestCase]
+%%   A test specification.
+%% TestCase = ConfCase | atom()
+%%   Configuration case, or the name of a test case function.
+%% ConfCase = {conf,Init,Spec,End} |
+%%            {conf,Properties,Init,Spec,End}
+%% Init = End = {Mod,Func} | Func
+%%   Initialization and cleanup function.
+%% Mod = Func = atom()
+%% Properties = [parallel | sequence | Shuffle | {RepeatType,N}]
+%%   Execution properties of the test cases (may be combined).
+%% Shuffle = shuffle | {shuffle,Seed}
+%%   To get cases executed in random order.
+%% Seed = {integer(),integer(),integer()}
+%% RepeatType = repeat | repeat_until_all_ok | repeat_until_all_fail |
+%%              repeat_until_any_ok | repeat_until_any_fail
+%%   To get execution of cases repeated.
+%% N = integer() | forever
+%% Reason = term()
+%%   The reason for skipping the test suite.
+%%
+%% @spec all(Clause) -> TestCases
+%% @end
+%%--------------------------------------------------------------------
+all() -> 
+    [test_get, test_put, test_state, test_modify, test_gets].
+
+
+%%--------------------------------------------------------------------
+%% TEST CASES
+%%--------------------------------------------------------------------
+
+%%--------------------------------------------------------------------
+%% @doc
+%%  Test case function. Returns a description of the test
+%%  case (doc), then returns a test specification (suite),
+%%  or performs the actual test (Config).
+%%
+%% Arg = doc | suite | Config
+%%   Indicates expected behaviour and return value.
+%% Config = [tuple()]
+%%   A list of key/value pairs, holding the test case configuration.
+%% Descr = [string()] | []
+%%   String that describes the test case.
+%% Spec = [tuple()] | []
+%%   A test specification, see all/1.
+%% Reason = term()
+%%   The reason for skipping the test case.
+%%
+%% @spec TestCase(Arg) -> Descr | Spec | ok | exit() | {skip,Reason}
+%% @end
+%%--------------------------------------------------------------------
+test_get(_Config) -> 
+    M = monad_state:get(),
+    M1 = cont_t:eval(M),
+    M2 = reader_t:run(M, undefined),
+    M3 = writer_t:eval(M),
+    ?assertEqual({ok, 5}, error_m:run(reader_t:run(state_t:eval(M, 5), 5))),
+    ?assertEqual({ok, 5}, error_m:run(reader_t:run(state_t:eval(M1, 5), 5))),
+    ?assertEqual({ok, 5}, error_m:run(reader_t:run(state_t:eval(M2, 5), 5))),
+    ?assertEqual({ok, 5}, error_m:run(reader_t:run(state_t:eval(M3, 5), 5))).
+
+test_put(_Config) ->
+    M = monad_state:put(10),
+    M1 = cont_t:eval(M),
+    M2 = reader_t:run(M, undefined),
+    M3 = writer_t:eval(M),
+    ?assertEqual({ok, 10}, error_m:run(reader_t:run(state_t:exec(M, 5), 5))),
+    ?assertEqual({ok, 10}, error_m:run(reader_t:run(state_t:exec(M1, 5), 5))),
+    ?assertEqual({ok, 10}, error_m:run(reader_t:run(state_t:exec(M2, 5), 5))),
+    ?assertEqual({ok, 10}, error_m:run(reader_t:run(state_t:exec(M3, 5), 5))).
+           
+test_state(_Config) ->
+    M = monad_state:state(fun(S) -> {S + 1, S} end), 
+    M1 = cont_t:eval(M),
+    M2 = reader_t:run(M, undefined),
+    M3 = writer_t:eval(M),
+    ?assertEqual({6, 5}, identity:run(state_t:run(M, 5))),
+    ?assertEqual({6, 5}, identity:run(state_t:run(M1, 5))),
+    ?assertEqual({6, 5}, identity:run(state_t:run(M2, 5))),
+    ?assertEqual({6, 5}, identity:run(state_t:run(M3, 5))).
+    
+test_modify(_Config) ->
+    M = monad_state:modify(fun(S) -> S + 1 end),
+    ?assertEqual({ok, 6}, identity:run(state_t:run(M, 5))).
+
+test_gets(_Config) ->
+    M = monad_state:gets(fun(A) -> 2 * A end),
+    ?assertEqual({10, 5}, identity:run(state_t:run(M, 5))).
