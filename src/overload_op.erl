37846d657f6de75ba3475cfa9bbd2d658dcbf5b6
slepher <cxyfree@gmail.com>
2019-01-24 15:42:41 +0800

    update overload_op

--- src/overload_op.erl
+++ src/overload_op.erl
@@ -9,6 +9,9 @@
 -module(overload_op).
 
 %% API
+
+-include_lib("astranaut/include/quote.hrl").
+
 -export([parse_transform/2]).
 
 %%%===================================================================
@@ -17,7 +20,7 @@
 
 parse_transform(Forms, _Opts) ->
     ParseOps = parse_ops(Forms),
-    ast_traverse:map(fun(Type, Node) -> walk(Type, Node, ParseOps) end, Forms).
+    astranaut_traverse:map(fun(Node, Attr) -> walk(Node, Attr, ParseOps) end, Forms, #{traverse => pre}).
 
 %%--------------------------------------------------------------------
 %% @doc
@@ -30,26 +33,34 @@ parse_transform(Forms, _Opts) ->
 %%%===================================================================
 
 parse_ops(Forms) ->
-    Overloads = ast_traverse:attributes(overloads, Forms),
+    Overloads = astranaut:attributes(overloads, Forms),
     lists:foldl(
       fun(Overload, Acc0) ->
               lists:foldl(
-                fun({Module, Ops}, Acc1) when is_list(Ops) ->
-                        lists:foldl(
-                          fun(Op, Acc2) ->
-                                  maps:put(Op, Module, Acc2)
-                          end, Acc1, Ops);
-                   ({Module, Op}, Acc1) when is_atom(Op) ->
-                        maps:put(Op, Module, Acc1)
+                fun({Module, Ops}, Acc1) ->
+                        add_op(Module, Ops, Acc1);
+                   (Ops, Acc1) ->
+                        add_op(undefined, Ops, Acc1)
                 end, Acc0, Overload)
       end, maps:new(), Overloads).
 
-walk(pre, {op, Line ,'/', {op, Line1, '/', A , {atom, _Line2, Op} = OpFun}, B} = Node, Ops) ->
+add_op(Module, Ops, Map) when is_list(Ops) ->
+    lists:foldl(
+      fun(Op, Acc) ->
+              maps:put(Op, Module, Acc)
+      end, Map, Ops);
+add_op(Module, Op, Map) when is_atom(Op) ->
+    add_op(Module, [Op], Map).
+
+walk({op, Line ,'/', {op, _Line1, '/', A , {atom, _Line2, Op} = OpFun}, B} = Node, _Attr, Ops) ->
     case maps:find(Op, Ops) of
+        {ok, undefined} ->
+            quote(_@OpFun(unquote(A), unquote(B)), Line);
         {ok, Module} ->
-            {call, Line, {remote, Line1, {atom, Line1, Module}, OpFun}, [A, B]};
+            quote(_A@Module:_@OpFun(unquote(A), unquote(B)), Line);
+        %{call, Line, {remote, Line1, {atom, Line1, Module}, OpFun}, [A, B]};
         error ->
             Node
     end;
-walk(_Type, Node, _Ops) ->
+walk(Node, _Attr, _Ops) ->
     Node.
---
+++ test/overload_op_SUITE.erl
@@ -0,0 +1,130 @@
+%%%-------------------------------------------------------------------
+%%% @author Chen Slepher <slepheric@gmail.com>
+%%% @copyright (C) 2019, Chen Slepher
+%%% @doc
+%%%
+%%% @end
+%%% Created : 24 Jan 2019 by Chen Slepher <slepheric@gmail.com>
+%%%-------------------------------------------------------------------
+-module(overload_op_SUITE).
+
+-compile(export_all).
+
+-include_lib("eunit/include/eunit.hrl").
+-include_lib("common_test/include/ct.hrl").
+
+%%--------------------------------------------------------------------
+%% @spec suite() -> Info
+%% Info = [tuple()]
+%% @end
+%%--------------------------------------------------------------------
+suite() ->
+    [{timetrap,{seconds,30}}].
+
+%%--------------------------------------------------------------------
+%% @spec init_per_suite(Config0) ->
+%%     Config1 | {skip,Reason} | {skip_and_save,Reason,Config1}
+%% Config0 = Config1 = [tuple()]
+%% Reason = term()
+%% @end
+%%--------------------------------------------------------------------
+init_per_suite(Config) ->
+    Config.
+
+%%--------------------------------------------------------------------
+%% @spec end_per_suite(Config0) -> term() | {save_config,Config1}
+%% Config0 = Config1 = [tuple()]
+%% @end
+%%--------------------------------------------------------------------
+end_per_suite(_Config) ->
+    ok.
+
+%%--------------------------------------------------------------------
+%% @spec init_per_group(GroupName, Config0) ->
+%%               Config1 | {skip,Reason} | {skip_and_save,Reason,Config1}
+%% GroupName = atom()
+%% Config0 = Config1 = [tuple()]
+%% Reason = term()
+%% @end
+%%--------------------------------------------------------------------
+init_per_group(_GroupName, Config) ->
+    Config.
+
+%%--------------------------------------------------------------------
+%% @spec end_per_group(GroupName, Config0) ->
+%%               term() | {save_config,Config1}
+%% GroupName = atom()
+%% Config0 = Config1 = [tuple()]
+%% @end
+%%--------------------------------------------------------------------
+end_per_group(_GroupName, _Config) ->
+    ok.
+
+%%--------------------------------------------------------------------
+%% @spec init_per_testcase(TestCase, Config0) ->
+%%               Config1 | {skip,Reason} | {skip_and_save,Reason,Config1}
+%% TestCase = atom()
+%% Config0 = Config1 = [tuple()]
+%% Reason = term()
+%% @end
+%%--------------------------------------------------------------------
+init_per_testcase(_TestCase, Config) ->
+    Config.
+
+%%--------------------------------------------------------------------
+%% @spec end_per_testcase(TestCase, Config0) ->
+%%               term() | {save_config,Config1} | {fail,Reason}
+%% TestCase = atom()
+%% Config0 = Config1 = [tuple()]
+%% Reason = term()
+%% @end
+%%--------------------------------------------------------------------
+end_per_testcase(_TestCase, _Config) ->
+    ok.
+
+%%--------------------------------------------------------------------
+%% @spec groups() -> [Group]
+%% Group = {GroupName,Properties,GroupsAndTestCases}
+%% GroupName = atom()
+%% Properties = [parallel | sequence | Shuffle | {RepeatType,N}]
+%% GroupsAndTestCases = [Group | {group,GroupName} | TestCase]
+%% TestCase = atom()
+%% Shuffle = shuffle | {shuffle,{integer(),integer(),integer()}}
+%% RepeatType = repeat | repeat_until_all_ok | repeat_until_all_fail |
+%%              repeat_until_any_ok | repeat_until_any_fail
+%% N = integer() | forever
+%% @end
+%%--------------------------------------------------------------------
+groups() ->
+    [].
+
+%%--------------------------------------------------------------------
+%% @spec all() -> GroupsAndTestCases | {skip,Reason}
+%% GroupsAndTestCases = [{group,GroupName} | TestCase]
+%% GroupName = atom()
+%% TestCase = atom()
+%% Reason = term()
+%% @end
+%%--------------------------------------------------------------------
+all() -> 
+    [overload_op].
+
+%%--------------------------------------------------------------------
+%% @spec TestCase() -> Info
+%% Info = [tuple()]
+%% @end
+%%--------------------------------------------------------------------
+overload_op() -> 
+    [].
+
+%%--------------------------------------------------------------------
+%% @spec TestCase(Config0) ->
+%%               ok | exit() | {skip,Reason} | {comment,Comment} |
+%%               {save_config,Config1} | {skip_and_save,Reason,Config1}
+%% Config0 = Config1 = [tuple()]
+%% Reason = term()
+%% Comment = term()
+%% @end
+%%--------------------------------------------------------------------
+overload_op(_Config) -> 
+    ?assertEqual(5, overload_op_example:test()).
