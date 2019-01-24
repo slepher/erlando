37f365b621791c5765e926645fb41fb6d16f7369
slepher <cxyfree@gmail.com>
2019-01-24 17:34:51 +0800

    get parse_transform function_generator back

--- include/gen_fun.hrl
+++ include/gen_fun.hrl
@@ -1,4 +1,6 @@
 -include_lib("astranaut/include/macro.hrl").
+-ifndef(ERLANDO_GEN_FUN).
+-define(ERLANDO_GEN_FUN, true).
 -use_macro({gen_fun_macro, gen_fun/2, [{attrs, [erlando_type]}, {as_attr, gen_fun}, 
                                        {auto_export, true}, {merge_function, head}]}).
-%-compile({parse_transform, function_generator}).
+-endif.
---
+++ src/function_generator.erl
@@ -0,0 +1,28 @@
+%%%-------------------------------------------------------------------
+%%% @author Chen Slepher <slepheric@gmail.com>
+%%% @copyright (C) 2017, Chen Slepher
+%%% @doc
+%%%
+%%% @end
+%%% Created : 25 Jul 2017 by Chen Slepher <slepheric@gmail.com>
+%%%-------------------------------------------------------------------
+-module(function_generator).
+
+%% API
+-export([parse_transform/2]).
+%%%===================================================================
+%%% API
+%%%===================================================================
+parse_transform(Forms, _Opts) ->
+    Opts =  [{attrs, [erlando_type]}, {as_attr, gen_fun}, {auto_export, true}, {merge_function, head}],
+    astranaut_macro:expand_macro(gen_fun_macro, gen_fun, 2, Opts, Forms).
+%%--------------------------------------------------------------------
+%% @doc
+%% @spec
+%% @end
+%%--------------------------------------------------------------------
+
+
+%%%===================================================================
+%%% Internal functions
+%%%===================================================================
