5f40b09e351df2df45757dfca109e1aa2743602a
slepher <cxyfree@gmail.com>
2020-11-10 17:34:36 +0800

    add applicative_lift

---
+++ src/instance/applicative_lift.erl
@@ -0,0 +1,100 @@
+%%%-------------------------------------------------------------------
+%%% @author Chen Slepher <slepheric@gmail.com>
+%%% @copyright (C) 2020, Chen Slepher
+%%% @doc
+%%%
+%%% @end
+%%% Created : 10 Nov 2020 by Chen Slepher <slepheric@gmail.com>
+%%%-------------------------------------------------------------------
+-module(applicative_lift).
+
+-erlando_type({lift, [lift/2]}).
+
+-export_type([lift/1, pure/1, other/1]).
+
+-define(TYPE, lift).
+
+%% This is really instance (Error e) => Monad (Either e) with 'error'
+%% for Left and 'ok' for Right.
+-type lift(A) :: pure(A) | other(A).
+
+-type pure(A) :: {pure, A}.
+-type other(_A) :: {other, term()}.
+
+-include("gen_fun.hrl").
+
+-behaviour(functor).
+-behaviour(applicative).
+
+-gen_fun(#{patterns => [lift], tbehaviours => [functor, applicative]}).
+
+%% API
+-export([fmap/2, '<$'/2]).
+-export([pure/1, '<*>'/2, lift_a2/3, '*>'/2, '<*'/2]).
+-export([run_errors/1, hoist_errors/1, failure/1]).
+-export([sequence_either/1]).
+%%%===================================================================
+%%% API
+%%%===================================================================
+
+%%--------------------------------------------------------------------
+%% @doc
+%% @spec
+%% @end
+%%--------------------------------------------------------------------
+fmap(F, LA) ->
+    case LA of
+        {pure, A} ->
+            {pure, F(A)};
+        {other, E} ->
+            {other, functor:fmap(F, E)}
+    end.
+
+-spec '<$'(B, lift(_A)) -> lift(B).
+'<$'(B, FA) ->
+    functor:'default_<$'(B, FA, ?TYPE).
+
+pure(A) ->
+    {pure, A}.
+
+-spec '<*>'(lift(fun((A) -> B)), lift(A)) -> lift(B).
+'<*>'({pure, F}, {pure, A}) ->
+    {pure, F(A)};
+'<*>'({pure, F}, {other, E}) ->
+    {other, functor:fmap(F, E)};
+'<*>'({other, EF}, {pure, A}) ->
+    {other, functor:fmap(fun(F) -> F(A) end, EF)};
+'<*>'({other, EF}, {other, E}) ->
+    {other, applicative:'<*>'(EF, E)}.
+
+-spec lift_a2(fun((A, B) -> C), lift(A), lift(B)) -> lift(C).
+lift_a2(F, EA, EB) ->
+    applicative:default_lift_a2(F, EA, EB, ?TYPE).
+
+-spec '*>'(lift(_A), lift(B)) -> lift(B).
+'*>'(EA, EB) ->
+    applicative:'default_*>'(EA, EB, ?TYPE).
+
+-spec '<*'(lift(A), lift(_B)) -> lift(A).
+'<*'(EA, EB) ->
+    applicative:'default_<*'(EA, EB, ?TYPE).
+
+run_errors({other, {const, E}}) ->
+    {left, E};
+run_errors({pure, A}) ->
+    {right, A}.
+
+hoist_errors({left, E}) ->
+    {other, {const, E}};
+hoist_errors({right, A}) ->
+    {pure, A}.
+
+failure(E) ->
+    {other, {const, E}}.
+
+sequence_either(FEA) ->
+    run_errors(undetermined:run(traversable:traverse(fun hoist_errors/1, FEA), lift)).
+
+%%%===================================================================
+%%% Internal functions
+%%%===================================================================
